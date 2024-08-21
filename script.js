484431e8b09dbf4aee1de14626bb64b866003a0c
Prannav Shankar <prannav.shankar@gmail.com>
2024-08-21 15:59:39 -0400

    completed ui for rock paper scissors game

--- script.js
+++ script.js
@@ -1,52 +1,85 @@
 function playGame() {
     let userScore = 0;
     let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Make your move! Rock, paper, or scissors?").trim().toLowerCase();
-        while (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-            alert("Invalid choice! Rock, paper, or scissors?");
-            playerSelection = prompt("Make your move! Rock, paper, or scissors?").trim().toLowerCase();
-        }
-        let result = playRound(playerSelection, computerSelection);
-        if (result === 1) {
-            userScore++;
-        }
-        else if (result === -1) {
-            computerScore++;
-        }
-        else {
-            continue;
-        }
-    }
+    let playerSelection = null;
+    const buttons = document.querySelectorAll("button");
+    const header = document.querySelector(".header");
+    const intro = document.createElement("h4");
+    intro.textContent = "Make your move! Rock, paper, or scissors?";
+    const compImg = document.getElementById("comp-img");
+    const userImg = document.getElementById("user-img");
+
+    header.appendChild(intro);
+    const score = document.querySelector("#score");
+    const message = document.querySelector("#message");
+    
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            intro.textContent = "Make your move! Rock, paper, or scissors?";
+            let computerSelection = getComputerChoice();
+            playerSelection = button.id
+
+            compImg.src = "./assets/" + computerSelection + ".webp";
+            userImg.src = "./assets/" + playerSelection + ".webp";
+
+            console.log(playerSelection)
+            if (button.id === 'finish') {
+                endGame(userScore, computerScore);
+
+                userScore = 0
+                computerScore = 0
+            }
+            else {
+                let result = playRound(playerSelection, computerSelection, message)
+                console.log(button.id)
+                if (result === 1) {
+                    userScore++;
+                }
+                else if (result === -1) {
+                    computerScore++;
+                }
+            }
+
+            score.textContent ="Record: "+ userScore + "-" + computerScore;
+
+        });
+    });
+
+function endGame(userScore, computerScore) {
+    userImg.src = "./assets/question.png"
+    compImg.src = "./assets/question.png"
     const scoreMsg = " You scored " + userScore + "-" + computerScore + " against the computer.";
     if (userScore > computerScore) {
-        alert("You won!" + scoreMsg);
+        message.textContent = "You won!" + scoreMsg;
     }
     else if (userScore < computerScore) {
-        alert("You lost" + scoreMsg);
+        message.textContent = "You lost" + scoreMsg;
     }
     else {
-        alert("Tie!" + scoreMsg);
+        message.textContent = "Tie!" + scoreMsg;
     }
+
+
+    intro.textContent = "Make a move to restart the game!"
+}
     
 }
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, message) {
     let scoreMsg = " You chose " + playerSelection + " and the computer chose " + computerSelection + ".";
     if (playerSelection == computerSelection) {
-        alert("Tie!" + scoreMsg);
+        message.textContent = "Tie!" + scoreMsg;
         return 0;
     }
     else if (
         (computerSelection == "paper" && playerSelection == "rock") || 
         (computerSelection == "rock" && playerSelection == "scissors") || 
         (computerSelection == "scissors" && playerSelection == "paper")) {
-        alert("You lost!" + scoreMsg);
+        message.textContent ="You lost!" + scoreMsg;
         return -1;
     }
 
     else {
-        alert("You win!" + scoreMsg);;
+        message.textContent = "You win!" + scoreMsg;
         return 1;
     }
 }
